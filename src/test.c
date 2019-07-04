b062d34261ccb67cc53b5985212df4d35b6634e4
ilia <ilia@ubuntu15>
2019-07-04 13:14:37 +0700

    Create

---
+++ src/test.c
@@ -0,0 +1,34 @@
+/*
+ ============================================================================
+ Name        : test.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+void func(void *ptr)
+{
+	int *num;
+	num=(int*)ptr;
+	printf("%d\n", *num);
+}
+
+int main(void)
+{
+	int a[5];
+	pthread_t tid[5];
+	for(int i=0; i<5;i++)
+	{
+		a[i]=i+1;
+		pthread_create(&tid[i], NULL,func, &a[i]);
+
+	}
+	for(int i=0;i<5;i++)
+		pthread_join(&tid[i], NULL);
+	return 1;
+}
